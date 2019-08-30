6c1ebc39067e54e09639f6798fbc7071e15853d9
svensis <11571941+Heldex@users.noreply.github.com>
2019-08-30 16:10:26 +0300

    Update cl_init.lua

--- stadek_checkpoint/cl_init.lua
+++ stadek_checkpoint/cl_init.lua
@@ -1,14 +1,6 @@
 include('shared.lua')
- 
---[[---------------------------------------------------------
-   Name: Draw
-   Purpose: Draw the model in-game.
-   Remember, the things you render first will be underneath!
----------------------------------------------------------]]
 
 function ENT:Draw()
-
-
 
 if self:GetNWString( "NoStealCode_PLS" ) == "Rebel" then
 
